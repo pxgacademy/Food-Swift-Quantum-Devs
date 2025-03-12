18055b97a2a04429de7a2d311c2a38fb1b78c64d
Mahdi Hassan <mahdioffi333@gmail.com>
2025-03-12 14:12:02 +0600

    added footer section 2 p tag

--- src/shared-components/footer/Footer.jsx
+++ src/shared-components/footer/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
     <div>
       <h3 className="text-xl text-center">..... FOOTER ..... </h3>
       <p>this is footer section</p>
+      <p>this is footer section p tag</p>
     </div>
   );
 };
