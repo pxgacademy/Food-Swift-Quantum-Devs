2d4a299e3db2716ef7eb76fbdeb5892b332a0bc9
Mahdi Hassan <mahdioffi333@gmail.com>
2025-03-14 22:56:26 +0600

    added card section

--- src/shared-components/footer/Footer.jsx
+++ src/shared-components/footer/Footer.jsx
@@ -4,8 +4,7 @@ const Footer = () => {
   return (
     <div>
       <h3 className="text-xl text-center">..... FOOTER ..... </h3>
-      <p>this is footer section</p>
-      <p>this is footer section p tag</p>
+      
     </div>
   );
 };
