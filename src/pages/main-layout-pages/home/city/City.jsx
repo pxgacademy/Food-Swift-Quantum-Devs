188812757b0ce0bb1da603d8518f7f66f6f4674c
Md.Atiqur Rahman <atiq9se@gmail.com>
2025-03-23 01:33:05 +0600

    city section design

--- src/pages/main-layout-pages/home/city/City.jsx
+++ src/pages/main-layout-pages/home/city/City.jsx
@@ -2,6 +2,11 @@ import cityOne from '../../../../assets/city/dhaka.webp'
 import cityTwo from '../../../../assets/city/chittagong.webp'
 import cityThree from '../../../../assets/city/sylhet.webp'
 import cityFour from '../../../../assets/city/rajshahi.webp'
+import cityFive from '../../../../assets/city/khulna.webp'
+import citySix from '../../../../assets/city/Barisal.webp'
+import citySeven from '../../../../assets/city/rangpur.webp'
+import cityEight from '../../../../assets/city/mymensingh.webp'
+
 const City = () => {
     return (
         <div className='lg:py-12 py-6'>
@@ -28,7 +33,31 @@ const City = () => {
                         <div className="rounded-md overflow-hidden">
                             <div className="restaurantImg relative">
                                 <img src={cityFour} alt="" />
-                                <h3 className='absolute top-2 right-2 text-black bg-amber-200 px-4 py-2 rounded-lg font-bold text-lg lg:text-2xl'>Dhaka</h3>
+                                <h3 className='absolute top-2 right-2 text-black bg-amber-200 px-4 py-2 rounded-lg font-bold text-lg lg:text-2xl'>Rajshahi</h3>
+                            </div>
+                        </div>
+                        <div className="rounded-md overflow-hidden">
+                            <div className="restaurantImg relative">
+                                <img src={cityFive} alt="" />
+                                <h3 className='absolute top-2 right-2 text-black bg-amber-200 px-4 py-2 rounded-lg font-bold text-lg lg:text-2xl'>Khulna</h3>
+                            </div>
+                        </div>
+                        <div className="rounded-md overflow-hidden">
+                            <div className="restaurantImg relative">
+                                <img src={citySix} alt="" />
+                                <h3 className='absolute top-2 right-2 text-black bg-amber-200 px-4 py-2 rounded-lg font-bold text-lg lg:text-2xl'>Barishal</h3>
+                            </div>
+                        </div>
+                        <div className="rounded-md overflow-hidden">
+                            <div className="restaurantImg relative">
+                                <img src={citySeven} alt="" />
+                                <h3 className='absolute top-2 right-2 text-black bg-amber-200 px-4 py-2 rounded-lg font-bold text-lg lg:text-2xl'>Rangpur</h3>
+                            </div>
+                        </div>
+                        <div className="rounded-md overflow-hidden">
+                            <div className="restaurantImg relative">
+                                <img src={cityEight} alt="" />
+                                <h3 className='absolute top-2 right-2 text-black bg-amber-200 px-4 py-2 rounded-lg font-bold text-lg lg:text-2xl'>Mymensingh</h3>
                             </div>
                         </div>
         
