04be2045bb63201808a810f386d8f5ca625dac2c
srshakil2 <rh3642921@gmail.com>
2025-03-24 04:04:42 +0600

    details page all ok

--- src/pages/main-layout-pages/home/FoodDetails/FoodDetails.jsx
+++ src/pages/main-layout-pages/home/FoodDetails/FoodDetails.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+
 const FoodDetails = () => {
+  const [quentity, setQuentity] = useState(1);
+  const handelOdd = () => {
+    if (quentity === 1) return;
+    setQuentity(quentity - 1);
+  };
+  const handelEven = () => {
+    setQuentity(quentity + 1);
+  };
   return (
-    <div className="grid md:grid-cols-3 gap-5 text-black dark:text-white">
+    <div className="grid md:grid-cols-3 gap-5 text-black dark:text-white mb-10">
+      {/* immag and description section */}
       <div className="md:col-span-2">
         {/* immag */}
-        <div className="">
+        <div>
           <img
             className="w-full "
             src="https://i.ibb.co.com/gXwM72D/restaurant-detail-big-img1-3.webp"
@@ -11,7 +22,7 @@ const FoodDetails = () => {
           />
         </div>
         {/* text and contity */}
-        <div className="flex items-center justify-between ">
+        <div className="flex items-center justify-between mt-10">
           <div className="mb-5 space-y-2">
             <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold">
               Nik Baker's
@@ -20,9 +31,57 @@ const FoodDetails = () => {
               Greater Kailash (GK) 2 Domino's
             </p>
           </div>
-          <p>rating section</p>
+          {/* reting */}
+          <div className="rating">
+            <input
+              type="radio"
+              name="rating-2"
+              className="mask mask-star-2 bg-orange-400"
+              aria-label="1 star"
+            />
+            <input
+              type="radio"
+              name="rating-2"
+              className="mask mask-star-2 bg-orange-400"
+              aria-label="2 star"
+            />
+            <input
+              type="radio"
+              name="rating-2"
+              className="mask mask-star-2 bg-orange-400"
+              aria-label="3 star"
+            />
+            <input
+              type="radio"
+              name="rating-2"
+              className="mask mask-star-2 bg-orange-400"
+              aria-label="4 star"
+              defaultChecked
+            />
+            <input
+              type="radio"
+              name="rating-2"
+              className="mask mask-star-2 bg-orange-400"
+              aria-label="5 star"
+            />
+          </div>
         </div>
         <p className="text-xl font-bold">$99.00</p>
+
+        <fieldset className="fieldset flex gap-3 mb-3">
+          <button onClick={handelOdd} className="text-4xl cursor-pointer">
+            -
+          </button>
+          <input
+            type="number"
+            className="input w-[60px] text-center text-xl"
+            value={quentity}
+          />
+          <button onClick={handelEven} className="text-3xl cursor-pointer">
+            +
+          </button>
+        </fieldset>
+
         <p className="text-sm text-gray-700 dark:text-white">
           Rennie White, an advertising manager in Dhaka City, leaves for work 10
           minutes later these days because she no longer has to wait in line for
@@ -35,13 +94,16 @@ const FoodDetails = () => {
           optio cumque nihilares impedit quo repellendus eligendi optio cumque
           nihilare impedit quo minus id quod maxime.
         </p>
+        {/* Order Now btn */}
         <div className="mt-5 md:mt-8">
-          <button className="btn text-xl">Order Now</button>
+          <button className="btn text-xl text-white md:text-2xl bg-primaryColor hover:bg-[#F29F05]border-none w-full">
+            Order Now
+          </button>
         </div>
       </div>
-      {/* Price */}
+      {/* Order and Price section */}
       <div className="md:col-span-1 bg-gradient-to-r from-red-500 via-green-950 to-cyan-600 p-[1px]">
-        <div className="h-full w-full bg-white dark:bg-black">
+        <div className="h-full w-full bg-white dark:bg-black relative">
           <h3 className="text-2xl text-center pt-5">Your Order</h3>
           {/* 1 Chicken tandori Special */}
           <div className="px-3 mt-7">
@@ -103,6 +165,27 @@ const FoodDetails = () => {
                 <p>Butter</p>
               </div>
               <p className="font-bold">$12.00</p>
+            </div>
+          </div>
+          {/* prement btn */}
+          <div className="mt-12 px-3 md:absolute bottom-0 bg-black dark:bg-white w-full text-white dark:text-black py-5">
+            <p>
+              SubTotal
+              <span className="float-right">$170.00</span>
+            </p>
+            <p>
+              Delivery
+              <span className="float-right">$5.00</span>
+            </p>
+            <p className="w-full border border-b-2 mt-3"></p>
+            <p>
+              Total
+              <span className="float-right">$175.00</span>
+            </p>
+            <div className="flex justify-center mt-5 w-full">
+              <button className="btn text-lg text-white md:text-xl bg-primaryColor hover:bg-[#F29F05] mt-5 border-none w-full">
+                confirm order
+              </button>
             </div>
           </div>
         </div>
