19590cc5d0b437e3f1616ff16971897eb7ca9348
Md.Atiqur Rahman <atiq9se@gmail.com>
2025-03-24 20:04:05 +0600

    food item add

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,15 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Routes";
 import MainContext from "./contexts/MainContext";
 
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// const queryClient = new QueryClient();
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <QueryClientProvider client={queryClient}> */}
+    <QueryClientProvider client={queryClient}>
       <MainContext>
         <RouterProvider router={router} />
       </MainContext>
-    {/* </QueryClientProvider> */}
+    </QueryClientProvider>
   </StrictMode>
 );
