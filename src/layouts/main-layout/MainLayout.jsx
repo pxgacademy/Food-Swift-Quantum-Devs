d53c879639380c967d676f0457154fc9614db951
Mahdi Hassan <mahdioffi333@gmail.com>
2025-03-26 15:49:52 +0600

    added popup modal

--- src/layouts/main-layout/MainLayout.jsx
+++ src/layouts/main-layout/MainLayout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../../shared-components/header/Header";
 import Footer from "../../shared-components/footer/Footer";
+import Modal from "../../shared-components/modal/Modal";
 
 const MainLayout = () => {
   return (
     <>
+      <Modal />
       <Header />
       <Outlet />
       <Footer></Footer>
