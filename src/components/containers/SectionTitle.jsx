ca63a2dfdf24cbacbc240173aecb10b4e631e66c
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-05-06 15:52:30 +0600

    Header, Footer, Banner Section, How to Work section updated, and added a new banner section

--- src/components/containers/SectionTitle.jsx
+++ src/components/containers/SectionTitle.jsx
@@ -2,9 +2,11 @@ import PropTypes from "prop-types";
 
 const SectionTitle = ({ children }) => {
   return (
-    <h2 className="text-2xl md:text-3xl text-primaryColor font-bold text-center mb-8">
-      {children}
-    </h2>
+    <div className="flex justify-center">
+      <h2 className="inset-shadow-[0px_0px_10px_1px] inset-shadow-primary/60 shadow-[6px_6px_15px_0px] shadow-black/20 py-3 px-7 rounded-md text-2xl md:text-3xl text-primary font-extrabold text-center mb-8 font-Lato">
+        {children}
+      </h2>
+    </div>
   );
 };
 
