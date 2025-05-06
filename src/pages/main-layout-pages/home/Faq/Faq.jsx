1e3deae69cec3b0d4010c20a9e71a3670d2987d9
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-05-06 20:11:05 +0600

    Updated FAQ section

--- src/pages/main-layout-pages/home/Faq/Faq.jsx
+++ src/pages/main-layout-pages/home/Faq/Faq.jsx
@@ -2,91 +2,88 @@ import { useState } from "react";
 import { BsArrowRight, BsArrowRightShort } from "react-icons/bs";
 import { FiMinus, FiPlus } from "react-icons/fi";
 import SectionContainer from "../../../../components/containers/SectionContainer";
+const faqs = [
+  {
+    question: "I got the wrong food. What should I do?",
+    ans: "Please contact our Customer Service Manager with a photo of the food box you ate.",
+  },
+  {
+    question: "My foodman’s number is unreachable. What should I do?",
+    ans: "Please contact our help center and tell us about your problem. Hopefully you will find a solution to your problem there.",
+  },
+  {
+    question: "The foodman refused to take my order. What can I do?",
+    ans: "You can complain to our help center with the name of that foodman and his phone number and order the food you want to get from our app.",
+  },
+  {
+    question: "The foodman cancelled my order. What should I do?",
+    ans: "You can complain to our Help Center by telling us what location and when your order is being canceled.",
+  },
+  {
+    question:
+      "I forgot to apply promo code on my food order. What can I do now?",
+    ans: "Then you go to our help center and tell us about your problem. Then if you are interested in getting the food, give us your phone number.",
+  },
+  {
+    question:
+      "I want to order from a restaurant that is not in the app. Possible?",
+    ans: "No, it is not possible. Because we only allow entry of all good quality restaurants in our app. We do not allow any local restaurants in our app.",
+  },
+];
+const foodMan = [
+  {
+    question: "The ordered food is not available. What to do?",
+    ans: "You will receive your food in a few moments. Our foodman is now on the road to your address. After this, I will inform your foodman about your star again.",
+  },
+  {
+    question: "My customer’s number is unreachable. What should I do?",
+    ans: "Please take a picture of the box for which you are not getting the customer number and send it to our Help Center.",
+  },
+  {
+    question: "My customer cancelled the order. What should I do?",
+    ans: "Take a picture of the box of the customer who canceled the order and submit it to our Help Center.",
+  },
+  {
+    question: "Restaurant not available temporarily. What should I do?",
+    ans: "You give that customer's number to our help center and tell them the problem.",
+  },
+  {
+    question:
+      "Customer wanted the food from different restaurant. Should I proceed?",
+    ans: "No, you know that we do not allow local restaurants to enter our app. You should report the problem to our help center.",
+  },
+  {
+    question: "The delivery address is more than 3 km? Should I deliver?",
+    ans: "Yes, you will deliver, but before that, you must inform the customer that if the distance is more than 3 kilometers, he will have to pay extra delivery.",
+  },
+];
 
 const Faq = () => {
   const [activeTab, setActiveTab] = useState("customer");
   const [openFAQ, setOpenFAQ] = useState(null);
-  const faqs = [
-    {
-      question: "I got the wrong food. What should I do?",
-      ans: "Please contact our Customer Service Manager with a photo of the food box you ate.",
-    },
-    {
-      question: "My foodman’s number is unreachable. What should I do?",
-      ans: "Please contact our help center and tell us about your problem. Hopefully you will find a solution to your problem there.",
-    },
-    {
-      question: "The foodman refused to take my order. What can I do?",
-      ans: "You can complain to our help center with the name of that foodman and his phone number and order the food you want to get from our app.",
-    },
-    {
-      question: "The foodman cancelled my order. What should I do?",
-      ans: "You can complain to our Help Center by telling us what location and when your order is being canceled.",
-    },
-    {
-      question:
-        "I forgot to apply promo code on my food order. What can I do now?",
-      ans: "Then you go to our help center and tell us about your problem. Then if you are interested in getting the food, give us your phone number.",
-    },
-    {
-      question:
-        "I want to order from a restaurant that is not in the app. Possible?",
-      ans: "No, it is not possible. Because we only allow entry of all good quality restaurants in our app. We do not allow any local restaurants in our app.",
-    },
-  ];
-  const foodMan = [
-    {
-      question: "The ordered food is not available. What to do?",
-      ans: "You will receive your food in a few moments. Our foodman is now on the road to your address. After this, I will inform your foodman about your star again.",
-    },
-    {
-      question: "My customer’s number is unreachable. What should I do?",
-      ans: "Please take a picture of the box for which you are not getting the customer number and send it to our Help Center.",
-    },
-    {
-      question: "My customer cancelled the order. What should I do?",
-      ans: "Take a picture of the box of the customer who canceled the order and submit it to our Help Center.",
-    },
-    {
-      question: "Restaurant not available temporarily. What should I do?",
-      ans: "You give that customer's number to our help center and tell them the problem.",
-    },
-    {
-      question:
-        "Customer wanted the food from different restaurant. Should I proceed?",
-      ans: "No, you know that we do not allow local restaurants to enter our app. You should report the problem to our help center.",
-    },
-    {
-      question: "The delivery address is more than 3 km? Should I deliver?",
-      ans: "Yes, you will deliver, but before that, you must inform the customer that if the distance is more than 3 kilometers, he will have to pay extra delivery.",
-    },
-  ];
+
   return (
-  <SectionContainer>
-    <h2 className="text-center lg:text-5xl md:text-3xl text-2xl mb-10">
+    <SectionContainer>
+      <h2 className="text-center lg:text-5xl md:text-3xl text-2xl mb-10">
         Frequently Asked Questions
       </h2>
       {/* tabs */}
       <div className="flex gap-2 justify-center mb-5">
         <button
-          className={`px-4 py-2 text-xl rounded ${
-            activeTab === "customer"
-              ? "bg-primaryColor hover:bg-[#F29F05] text-white"
-              : "bg-gray-200 dark:text-black"
+          className={`btn ${
+            activeTab === "customer" ? "btn-primary" : "btn-ghost"
           }`}
           onClick={() => setActiveTab("customer")}
         >
-          I’m a customer
+          I&apos;m a customer
         </button>
         <button
-          className={`px-4 py-2 text-xl rounded ${
-            activeTab === "foodman"
-              ? "bg-primaryColor hover:bg-[#F29F05] text-white"
-              : "bg-gray-200 dark:text-black"
+          className={`btn ${
+            activeTab === "foodman" ? "btn-primary" : "btn-ghost"
           }`}
           onClick={() => setActiveTab("foodman")}
         >
-          I’m a Food man
+          I&apos;m a Foodman
         </button>
       </div>
       {/* FAQ section */}
@@ -135,17 +132,8 @@ const Faq = () => {
           </div>
         )}
       </div>
-      {/* btn view all FAQ */}
-      <div className="flex items-center justify-center mt-10">
-        <button className="flex items-center justify-between btn text-2xl text-white font-normal bg-primaryColor hover:bg-[#F29F05]">
-          View all FAQ
-          <span className="text-red-500 ml-2">
-            <BsArrowRight />
-          </span>
-        </button>
-      </div>
-  </SectionContainer>
-    
+      
+    </SectionContainer>
   );
 };
 
