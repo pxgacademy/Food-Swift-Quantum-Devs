8126edfaa2b1cab656ba12b6e4e8fc7e2cb483e0
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-05-22 22:29:56 +0600

    Project converted jsx to tsx

--- src/hooks/useContextValue.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { ContextProvider } from "../contexts/ContextProvider";
-
-const useContextValue = () => {
-  const context = useContext(ContextProvider);
-  return context;
-};
-
-export default useContextValue;
--- src/hooks/useThemeStore.jsx
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("loizar-theme") || "coffee",
-  setTheme: (theme) => {
-    localStorage.setItem("loizar-theme", theme);
-    set({ theme });
-  },
-}));
