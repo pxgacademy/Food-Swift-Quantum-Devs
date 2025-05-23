8fdd271ae9903901dd86e6274e14047c7b74f9f4
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-05-24 01:01:18 +0600

    added header section

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,65 @@
+import { Link } from "react-router-dom";
+import { User } from "lucide-react";
+import deliveryIcon from "/food-delivery.png";
+
+export default function Header() {
+  // TODO:
+  const user: { email: string; photoURL?: string } = { email: "" };
+
+  return (
+    <nav className="border-b border-primary/30 py-3">
+      <div className="container mx-auto flex justify-between">
+        {/* left side nav */}
+        <Link to="/">
+          <div className="flex items-center">
+            <img src={deliveryIcon} alt="logo" className="size-8 sm:size-10" />
+            <h2 className="text-lg sm:text-2xl md:text-2xl bg-base-100 px-5 py-1 rounded font-Lobster text-primary">
+              Loizar
+            </h2>
+          </div>
+        </Link>
+
+        <div className="flex items-center gap-2">
+          <button className="hidden sm:flex btn">
+            <span className="hidden md:inline">Select</span> Your Location
+          </button>
+
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar text-center"
+            >
+              <div className="size-7 sm:size-10 rounded-full">
+                {user?.photoURL ? (
+                  <img alt="" src={user?.photoURL} className="rounded-full" />
+                ) : (
+                  <span className="w-full h-full flex items-center justify-center border border-primary/40 rounded-full">
+                    <User size={32} />
+                  </span>
+                )}
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-200 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              {user && (
+                <li>
+                  <a className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </a>
+                </li>
+              )}
+              <li>
+                <a>Settings</a>
+              </li>
+              {user && <li>{/* <Logout /> */}</li>}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
--- src/layouts/main-layout/MainLayout.tsx
+++ src/layouts/main-layout/MainLayout.tsx
@@ -1,5 +1,6 @@
 
 import { Outlet } from "react-router-dom";
+import Header from "../../components/header/Header";
 
 
 
@@ -7,6 +8,7 @@ export default function MainLayout() {
   
   return (
     <section data-theme='coffee'>
+      <Header/>
       <Outlet />
     </section>
   );
